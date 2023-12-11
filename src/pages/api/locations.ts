f84c2c167227672453e8272d35f695ce453eedbe
akashkharde <akashkharde111@gmail.com>
2023-12-11 10:39:56 +0530

    --\

--- src/pages/api/locations.ts
+++ src/pages/api/locations.ts
@@ -1,17 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-
 function errorRes(res: NextApiResponse, status: number, message: string) {
   return res.status(status).json({ message });
 }
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   if (req.method !== 'GET') {
     return errorRes(res, 405, 'Method not allowed');
   }
 
   const { startsWith, limit }: { startsWith?: string; limit?: string } = req.query as { startsWith?: string; limit?: string };
-
 
   if (!startsWith) {
     return errorRes(res, 400, 'startsWith parameter is required');
@@ -26,6 +24,11 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (Number.isNaN(limitNum)) {
     return errorRes(res, 400, 'limit parameter must be of type number');
   }
+
+  // Perform other asynchronous operations as needed
+
+  // Ensure a value is returned at the end
+  return Promise.resolve(); // You can also return an actual value if needed
 }
 
 export default handler;
