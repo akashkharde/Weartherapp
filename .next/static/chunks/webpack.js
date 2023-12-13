62bbeb4f51de284a315c554b2fadf58a27b550cc
akashkharde <akashkharde111@gmail.com>
2023-12-14 00:08:53 +0530

    changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c7f37017c0ce9a2e"; }
+/******/ 		__webpack_require__.h = function() { return "36064e2af2366b33"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -2,31 +2,10 @@ import weatherworksLogoImage from '@/assets/images/logos/weatherworks.png';
 import Container from '@/components/Container';
 import Image from 'next/image';
 import Link from 'next/link';
-import { IoMailUnreadSharp } from 'react-icons/io5';
 
 
 function Header() {
 
-  const socials = [
-    {
-      position: 0,
-      title: 'View GitHub profile',
-      icon: <IoMailUnreadSharp />,
-      link: 'https://www.google.com/',
-    },
-    {
-      position: 1,
-      title: 'Join Discord server',
-      icon: <IoMailUnreadSharp />,
-      link: 'https://www.google.com/',
-    },
-    {
-      position: 2,
-      title: 'View source code',
-      icon: <IoMailUnreadSharp />,
-      link: 'https://www.google.com/',
-    },
-  ];
   return (
     <header className="w-full py-3">
       <Container>
@@ -41,20 +20,6 @@ function Header() {
               className="h-14 w-14 lg:h-16 lg:w-16"
             />
           </Link>
-          <div className="flex gap-x-2">
-            {socials.map((social) => (
-              <a
-                key={social.position}
-                href={social.link}
-                rel="noreferrer"
-                target="_blank"
-                title={social.title}
-                className="text-neutrals-50/70 transition-colors duration-200 focus-visible:text-neutrals-50 hover:text-neutrals-50"
-              >
-                <div className="h-10 w-10">{social.icon}</div>
-              </a>
-            ))}
-          </div>
         </div>
       </Container>
     </header>
